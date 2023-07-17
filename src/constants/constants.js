319926989d2aac7a4d01cf93be6487a140c4c1d1
Roshmeet Chakraborty <riju17chak@gmail.com>
2023-07-18 02:20:06 +0530

    rickroll :

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const CONSTANTS  = {
     'AUTHOR': 'Roshmeet Chakraborty',
-    'AUTHOR_GITHUB_LINK': 'https://github.com/C0D3R-M33T',
+    'AUTHOR_GITHUB_LINK': 'https://youtu.be/xvFZjo5PgG0',
     'AUTHOR_LINKEDIN_LINK': 'https://www.linkedin.com/in/roshmeet/',
 }
